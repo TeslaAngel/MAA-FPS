9ec98eb08ce2b1ec1620ab2c5ec81e80c3f9beff
TeslaAngel <42201165+TeslaAngel@users.noreply.github.com>
2020-04-12 18:59:40 +0800

    AAAA'a!

---
+++ Assets/Photon/PhotonChat/Demos/DemoChat/PictureTransformer.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+
+using UnityEngine.UI;
+
+
+public class PictureTransformer : MonoBehaviour
+{
+
+    public string ToImageString;
+    public string channelTag;
+    public Texture2D RP;
+
+    //base64转图片
+    public void Base64ToTexture2d(string Base64STR)
+    {
+        Texture2D pic = new Texture2D(1111, 1111);
+        byte[] data = System.Convert.FromBase64String(Base64STR);
+        pic.LoadImage(data);
+        //byte[] bytes = pic.EncodeToPNG();
+
+        //下面是为了方便了解图片的信息写的
+        string year = System.DateTime.Now.Year.ToString();
+        string month = System.DateTime.Now.Month.ToString();
+        string day = System.DateTime.Now.Day.ToString();
+        string hour = System.DateTime.Now.Hour.ToString();
+        string minute = System.DateTime.Now.Minute.ToString();
+        string secend = System.DateTime.Now.Second.ToString();
+        //存储路径
+        //string FileFullPath = Application.dataPath/*这是获取assets前的文件路径*/ + "/" + year + "-" + month + "-" + day + "-" + hour + "-" + minute + "-" + secend + ".png";
+
+        //File.WriteAllBytes(FileFullPath, bytes);
+        //return FileFullPath;
+        RP = pic;
+    }
+
+
+    //图片转base64string
+    public string Texture2dToBase64(string texture2d_path)
+    {
+        //将图片文件转为流文件
+        FileStream fs = new System.IO.FileStream(texture2d_path, System.IO.FileMode.Open, System.IO.FileAccess.Read);
+        byte[] thebytes = new byte[fs.Length];
+
+        fs.Read(thebytes, 0, (int)fs.Length);
+        //转为base64string
+        string base64_texture2d = Convert.ToBase64String(thebytes);
+        return base64_texture2d;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (ToImageString != null && GetComponent<Image>()!=null)
+        {
+            //string ffp = Base64ToTexture2d(ToImageString);
+            Base64ToTexture2d(ToImageString);
+            Sprite sprite = Sprite.Create(RP, new Rect(0, 0, RP.width, RP.height),new Vector2(0.5f, 0.5f),100);
+            GetComponent<Image>().overrideSprite = sprite;
+        }
+    }
+}
---
+++ Assets/SettingsScript.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsScript : MonoBehaviour
+{
+    public GameObject ChatPanel;
+    public GameObject SettingPanel;
+
+    public void SetResolution1280720()
+    {
+        FullScreenMode fullScreenMode = Screen.fullScreenMode;
+        Screen.SetResolution(1280, 720, fullScreenMode);
+    }
+
+    public void SetResolution19201080()
+    {
+        FullScreenMode fullScreenMode = Screen.fullScreenMode;
+        Screen.SetResolution(1920, 1080, fullScreenMode);
+    }
+
+    public void SetResolutionSD()
+    {
+        FullScreenMode fullScreenMode = Screen.fullScreenMode;
+        Screen.SetResolution(Screen.width, Screen.height, fullScreenMode);
+    }
+
+    public void SetFullScreenMode()
+    {
+        FullScreenMode fullScreenMode = Screen.fullScreenMode;
+        if (fullScreenMode != FullScreenMode.Windowed)
+        {
+            Screen.fullScreen = false;
+            return;
+        }
+        else
+        {
+            Screen.fullScreen = true;
+            return;
+        }
+    }
+
+    public void GoesSettings()
+    {
+        ChatPanel.SetActive(false);
+        SettingPanel.SetActive(true);
+    }
+
+    public void GoesChatting()
+    {
+        ChatPanel.SetActive(true);
+        SettingPanel.SetActive(false);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
